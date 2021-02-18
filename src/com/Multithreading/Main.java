bd5c4c6edf6f39e69347edf5548722a797a6fd9e
timofeya123 <timofeya123@inbox.ru>
2021-02-18 22:15:37 +0600

    Callable and FutureTask

--- src/com/Multithreading/Main.java
+++ src/com/Multithreading/Main.java
@@ -1,21 +1,46 @@
 package com.Multithreading;
 
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
 
+    //Conditions
     static Lock lock =new ReentrantLock();
     static Condition condition = lock.newCondition();
     static int account = 0;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        //Conditions
         new AccountPlus().start();
         new AccountMinus().start();
+
+        //Callable and FutureTask
+        Callable<Integer> callable = new MyCallable();  // создаем код, который должен быть в потоке
+        FutureTask futureTask = new FutureTask(callable); // запихиваем наш код в futureTask
+        new Thread(futureTask).start();         // создаем нужный нам поток и запускаем его, типа futureTask
+        System.out.println(futureTask.get()); // возвращает результат
     }
 
+    //Callable and FutureTask
+    static class MyCallable implements Callable{
+        @Override
+        public Object call() throws Exception { // возвращаемый поток
+            int j=0;
+            for (int i=0;i<10;i++) {
+                j++;
+                Thread.sleep(100);
+            }
+            return j;
+        }
+    }
+
+    //Conditions
     static class AccountPlus extends Thread{
         @Override
         public void run() {
@@ -26,6 +51,7 @@ public class Main {
         }
     }
 
+    //Conditions
     static class AccountMinus extends Thread{
         @Override
         public void run() {
